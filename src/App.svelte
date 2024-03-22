ba6a49428e3050be06b981cf186c4b02c87550ed
Coodi419 <myrururu01@gmail.com>
2024-03-22 10:28:54 +0900

    8_1_Main_Svelte_fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Router from 'svelte-spa-router'
 	import Home from "./routes/Home.svelte"
+	import Login from './Login.svelte';
   
 	const routes = {
 	  '/': Home,
+	  '/login.html': Login,
 	}
   </script>
   
