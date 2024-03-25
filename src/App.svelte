85ea98a7204d37b9564e2d1387885566e9bef8f9
Coodi419 <myrururu01@gmail.com>
2024-03-25 11:07:51 +0900

    backend_support

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,19 @@
 	import Login from './routes/Login.svelte';
 
 	export let url = "";
+	let number;
+	let inputNumber;
+
+	const request = async (endpoint) => {
+		const resp = await fetch(`http://localhost:8000/api${endpoint}`);
+		const result = await resp.json();
+		return result;
+	};
+
+	const getNumberFromBackend = async (no) => {
+		const result = await request(`/post/${no}`);
+		number = result.data;
+	};
   </script>
   
   <main>
@@ -11,4 +24,15 @@
 		<Route path="/" component={Home} />
 		<Route path="/login" component={Login} />
 	</Router>
+	<h1>Fast Blog</h1>
+	<p>We are building our blog using FastAPI and Svelte!</p>
+	<p>response number: {number || ''}</p>
+	<input bind:value={inputNumber}>
+	<button type="button" on:click={() => getNumberFromBackend(inputNumber)}>submit</button>
   </main>
+
+  <style>
+	h1 {
+		color: brown;
+	}
+  </style>
